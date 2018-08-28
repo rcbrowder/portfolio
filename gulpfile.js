b5163c034d4b0d0fd481280c5e385204ca336082
Chris Browder <rcbrowder@gmail.com>
2018-08-28 15:17:13 -0400

    Adds gulpfile, SASS file, and adds gulp-sass to package.json

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
