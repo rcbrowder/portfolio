bc60966dce48e337f22b9d2e04b8e12d81099fbf
Chris Browder <rcbrowder@gmail.com>
2018-08-28 16:09:05 -0400

    Uses gulp to process .scss files. Also working on chart styling.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
-var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('/.scss', ['sass']);
 });
